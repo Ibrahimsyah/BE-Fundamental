40264c13f91f1d850a28d545b1c04173400dba6d
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-06-07 11:36:33 +0700

    init project

---
+++ playground/src/server.js
@@ -0,0 +1,21 @@
+const hapi = require('@hapi/hapi');
+const routes = require('./api/notes/route');
+
+const init = async () => {
+  const server = hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
